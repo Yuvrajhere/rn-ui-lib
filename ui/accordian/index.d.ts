3732992b5ec8f921098c9e7e57f9362449bea706
yuvrajhere <yuvrajisbest13@gmail.com>
2024-12-30 12:49:35 +0530

    feat: add accordian

--- ui/accordian/index.d.ts
+++ ui/accordian/index.d.ts
@@ -1,16 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Svg } from 'react-native-svg';
+import { View, TextProps } from 'react-native';
 import type { VariantProps } from '@gluestack-ui/nativewind-utils';
+type IPrimitiveIcon = {
+    height?: number | string;
+    width?: number | string;
+    fill?: string;
+    color?: string;
+    size?: number | string;
+    stroke?: string;
+    as?: React.ElementType;
+    className?: string;
+    classNameColor?: string;
+};
 /** Components */
-declare const Accordion: React.ForwardRefExoticComponent<Omit<{
-    className?: string | undefined;
-} & import("react-native").ViewProps & {
+declare const Accordion: React.ForwardRefExoticComponent<Omit<Omit<import("react-native").ViewProps & {
     context?: any;
-} & React.RefAttributes<unknown> & React.RefAttributes<import("react-native").ViewProps & {
+} & React.RefAttributes<typeof View> & import("@gluestack-ui/accordion/lib/types").IAccordionProps, "ref"> & React.RefAttributes<import("react-native").ViewProps & {
     context?: any;
-} & React.RefAttributes<unknown>> & import("@gluestack-ui/accordion/lib/typescript/types").IAccordionProps & VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<{
+} & React.RefAttributes<typeof View>>, "ref"> & VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<{
     variant: {
         filled: string;
+        unfilled: string;
     };
     size: {
         sm: string;
@@ -20,6 +31,7 @@ declare const Accordion: React.ForwardRefExoticComponent<Omit<{
 } | ({
     variant: {
         filled: string;
+        unfilled: string;
     };
     size: {
         sm: string;
@@ -29,6 +41,9 @@ declare const Accordion: React.ForwardRefExoticComponent<Omit<{
 } & {
     variant: {
         filled: import("tailwind-merge").ClassNameValue | {
+            base?: import("tailwind-merge").ClassNameValue;
+        };
+        unfilled: import("tailwind-merge").ClassNameValue | {
             base?: import("tailwind-merge").ClassNameValue;
         };
     };
@@ -46,6 +61,7 @@ declare const Accordion: React.ForwardRefExoticComponent<Omit<{
 }), undefined, "w-full", import("tailwind-variants/dist/config").TVConfig<{
     variant: {
         filled: string;
+        unfilled: string;
     };
     size: {
         sm: string;
@@ -55,6 +71,7 @@ declare const Accordion: React.ForwardRefExoticComponent<Omit<{
 }, {
     variant: {
         filled: string;
+        unfilled: string;
     };
     size: {
         sm: string;
@@ -64,6 +81,7 @@ declare const Accordion: React.ForwardRefExoticComponent<Omit<{
 }>, {
     variant: {
         filled: string;
+        unfilled: string;
     };
     size: {
         sm: string;
@@ -73,6 +91,7 @@ declare const Accordion: React.ForwardRefExoticComponent<Omit<{
 }, undefined, import("@gluestack-ui/nativewind-utils/types").TVReturnType<{
     variant: {
         filled: string;
+        unfilled: string;
     };
     size: {
         sm: string;
@@ -82,6 +101,7 @@ declare const Accordion: React.ForwardRefExoticComponent<Omit<{
 }, undefined, "w-full", import("tailwind-variants/dist/config").TVConfig<{
     variant: {
         filled: string;
+        unfilled: string;
     };
     size: {
         sm: string;
@@ -91,34 +111,31 @@ declare const Accordion: React.ForwardRefExoticComponent<Omit<{
 }, {
     variant: {
         filled: string;
+        unfilled: string;
     };
     size: {
         sm: string;
         md: string;
         lg: string;
     };
-}>, unknown, unknown, undefined>>>, "ref"> & React.RefAttributes<unknown>>;
-declare const AccordionItem: React.ForwardRefExoticComponent<Omit<{
-    className?: string | undefined;
-} & import("react-native").ViewProps & React.RefAttributes<import("react-native").ViewProps> & import("@gluestack-ui/accordion/lib/typescript/types").IAccordionItemProps & VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<({} | {} | {}) & {
+}>, unknown, unknown, undefined>>> & React.RefAttributes<import("react-native").ViewProps & {
+    context?: any;
+} & React.RefAttributes<typeof View>>>;
+declare const AccordionItem: React.ForwardRefExoticComponent<Omit<import("react-native").ViewProps & import("@gluestack-ui/accordion/lib/types").IAccordionItemProps & React.RefAttributes<import("react-native").ViewProps>, "ref"> & VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<({} | {} | {}) & {
     variant: {
         filled: string;
         unfilled: string;
     };
-}, undefined, undefined, import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, {} | {}, undefined, import("@gluestack-ui/nativewind-utils/types").TVReturnType<unknown, undefined, undefined, import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, unknown, unknown, undefined>>>, "ref"> & React.RefAttributes<unknown>>;
-declare const AccordionContent: React.ForwardRefExoticComponent<Omit<{
-    className?: string | undefined;
-} & import("react-native").ViewProps & React.RefAttributes<import("react-native").ViewProps> & VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<{} | {} | {}, undefined, "px-5 mt-2 pb-5", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, {} | {}, undefined, import("@gluestack-ui/nativewind-utils/types").TVReturnType<unknown, undefined, "px-5 mt-2 pb-5", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, unknown, unknown, undefined>>>, "ref"> & React.RefAttributes<unknown>>;
-declare const AccordionContentText: React.ForwardRefExoticComponent<Omit<{
-    className?: string | undefined;
-} & import("react-native").TextProps & React.RefAttributes<import("react-native").TextProps> & VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<({} | {} | {}) & {
+}, undefined, undefined, import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, {} | {}, undefined, import("@gluestack-ui/nativewind-utils/types").TVReturnType<unknown, undefined, undefined, import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, unknown, unknown, undefined>>> & React.RefAttributes<import("react-native").ViewProps>>;
+declare const AccordionContent: React.ForwardRefExoticComponent<Omit<import("react-native").ViewProps & React.RefAttributes<import("react-native").ViewProps>, "ref"> & VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<{} | {} | {}, undefined, "px-5 mt-2 pb-5", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, {} | {}, undefined, import("@gluestack-ui/nativewind-utils/types").TVReturnType<unknown, undefined, "px-5 mt-2 pb-5", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, unknown, unknown, undefined>>> & React.RefAttributes<import("react-native").ViewProps>>;
+declare const AccordionContentText: React.ForwardRefExoticComponent<Omit<TextProps & React.RefAttributes<TextProps>, "ref"> & VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<({} | {} | {}) & {
     size: {
         sm: string;
         md: string;
         lg: string;
     };
-}, undefined, "text-typography-700 font-normal", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, {} | {}, undefined, import("@gluestack-ui/nativewind-utils/types").TVReturnType<unknown, undefined, "text-typography-700 font-normal", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, unknown, unknown, undefined>>>, "ref"> & React.RefAttributes<unknown>>;
-declare const AccordionIcon: React.ForwardRefExoticComponent<Omit<VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<({} | {} | {}) & {
+}, undefined, "text-typography-700 font-normal", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, {} | {}, undefined, import("@gluestack-ui/nativewind-utils/types").TVReturnType<unknown, undefined, "text-typography-700 font-normal", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, unknown, unknown, undefined>>> & React.RefAttributes<TextProps>>;
+declare const AccordionIcon: React.ForwardRefExoticComponent<VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<({} | {} | {}) & {
     size: {
         '2xs': string;
         xs: string;
@@ -127,27 +144,16 @@ declare const AccordionIcon: React.ForwardRefExoticComponent<Omit<VariantProps<i
         lg: string;
         xl: string;
     };
-}, undefined, "", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, {} | {}, undefined, import("@gluestack-ui/nativewind-utils/types").TVReturnType<unknown, undefined, "", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, unknown, unknown, undefined>>> & Omit<any, "ref"> & React.RefAttributes<unknown> & React.RefAttributes<Omit<any, "ref"> & React.RefAttributes<unknown>> & {
-    as?: any;
-} & {
-    className?: any;
-    fill?: string | undefined;
-    as?: any;
-    color?: string | undefined;
-}, "ref"> & React.RefAttributes<unknown>>;
-declare const AccordionHeader: React.ForwardRefExoticComponent<Omit<{
-    className?: string | undefined;
-} & Omit<import("react-native").TextProps & React.ClassAttributes<typeof Text>, "style"> & import("@expo/html-elements/build/primitives/Text").WebTextProps & React.RefAttributes<import("@expo/html-elements/build/primitives/Text").TextProps> & VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<{} | {} | {}, undefined, "mx-0 my-0", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, {} | {}, undefined, import("@gluestack-ui/nativewind-utils/types").TVReturnType<unknown, undefined, "mx-0 my-0", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, unknown, unknown, undefined>>>, "ref"> & React.RefAttributes<unknown>>;
-declare const AccordionTrigger: React.ForwardRefExoticComponent<Omit<{
-    className?: string | undefined;
-} & Omit<import("react-native").PressableProps & React.RefAttributes<View>, "children"> & import("@gluestack-ui/accordion/lib/typescript/types").IAccordionTriggerProps & VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<{} | {} | {}, undefined, "w-full py-5 px-5 flex-row justify-between items-center web:outline-none focus:outline-none data-[disabled=true]:opacity-40 data-[disabled=true]:cursor-not-allowed data-[focus-visible=true]:bg-background-50", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, {} | {}, undefined, import("@gluestack-ui/nativewind-utils/types").TVReturnType<unknown, undefined, "w-full py-5 px-5 flex-row justify-between items-center web:outline-none focus:outline-none data-[disabled=true]:opacity-40 data-[disabled=true]:cursor-not-allowed data-[focus-visible=true]:bg-background-50", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, unknown, unknown, undefined>>>, "ref"> & React.RefAttributes<unknown>>;
-declare const AccordionTitleText: React.ForwardRefExoticComponent<Omit<{
-    className?: string | undefined;
-} & import("react-native").TextProps & React.RefAttributes<import("react-native").TextProps> & VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<({} | {} | {}) & {
+}, undefined, "text-typography-900 fill-none", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, {} | {}, undefined, import("@gluestack-ui/nativewind-utils/types").TVReturnType<unknown, undefined, "text-typography-900 fill-none", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, unknown, unknown, undefined>>> & Omit<Omit<IPrimitiveIcon & React.RefAttributes<Svg>, "ref"> & React.RefAttributes<IPrimitiveIcon & React.RefAttributes<Svg>>, "ref"> & {
+    as?: React.ElementType;
+} & React.RefAttributes<IPrimitiveIcon & React.RefAttributes<Svg>>>;
+declare const AccordionHeader: React.ForwardRefExoticComponent<Omit<TextProps & React.RefAttributes<TextProps>, "ref"> & VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<{} | {} | {}, undefined, "mx-0 my-0", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, {} | {}, undefined, import("@gluestack-ui/nativewind-utils/types").TVReturnType<unknown, undefined, "mx-0 my-0", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, unknown, unknown, undefined>>> & React.RefAttributes<TextProps>>;
+declare const AccordionTrigger: React.ForwardRefExoticComponent<Omit<Omit<Omit<import("react-native").PressableProps & React.RefAttributes<View>, "children"> & import("@gluestack-ui/accordion/lib/types").IAccordionTriggerProps, "ref"> & React.RefAttributes<import("react-native").PressableProps & React.RefAttributes<View>>, "ref"> & VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<{} | {} | {}, undefined, "w-full py-5 px-5 flex-row justify-between items-center web:outline-none focus:outline-none data-[disabled=true]:opacity-40 data-[disabled=true]:cursor-not-allowed data-[focus-visible=true]:bg-background-50", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, {} | {}, undefined, import("@gluestack-ui/nativewind-utils/types").TVReturnType<unknown, undefined, "w-full py-5 px-5 flex-row justify-between items-center web:outline-none focus:outline-none data-[disabled=true]:opacity-40 data-[disabled=true]:cursor-not-allowed data-[focus-visible=true]:bg-background-50", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, unknown, unknown, undefined>>> & React.RefAttributes<import("react-native").PressableProps & React.RefAttributes<View>>>;
+declare const AccordionTitleText: React.ForwardRefExoticComponent<Omit<TextProps & React.RefAttributes<TextProps>, "ref"> & VariantProps<import("@gluestack-ui/nativewind-utils/types").TVReturnType<({} | {} | {}) & {
     size: {
         sm: string;
         md: string;
         lg: string;
     };
-}, undefined, "text-typography-900 font-bold flex-1 text-left", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, {} | {}, undefined, import("@gluestack-ui/nativewind-utils/types").TVReturnType<unknown, undefined, "text-typography-900 font-bold flex-1 text-left", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, unknown, unknown, undefined>>>, "ref"> & React.RefAttributes<unknown>>;
+}, undefined, "text-typography-900 font-bold flex-1 text-left", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, {} | {}, undefined, import("@gluestack-ui/nativewind-utils/types").TVReturnType<unknown, undefined, "text-typography-900 font-bold flex-1 text-left", import("tailwind-variants/dist/config").TVConfig<unknown, {} | {}>, unknown, unknown, undefined>>> & React.RefAttributes<TextProps>>;
 export { Accordion, AccordionItem, AccordionHeader, AccordionTrigger, AccordionTitleText, AccordionContentText, AccordionIcon, AccordionContent, };
--- ui/accordian/index.jsx
+++ ui/accordian/index.jsx
@@ -1,96 +1,115 @@
-"use client";
-import React, { useMemo } from "react";
-import { createAccordion } from "@gluestack-ui/accordion";
-import { Svg } from "react-native-svg";
-import { View, Pressable, Text, Platform } from "react-native";
-import { tva } from "@gluestack-ui/nativewind-utils/tva";
-import { withStyleContext, useStyleContext, } from "@gluestack-ui/nativewind-utils/withStyleContext";
-import { withStyleContextAndStates } from "@gluestack-ui/nativewind-utils/withStyleContextAndStates";
-import { H3 } from "@expo/html-elements";
-import { cssInterop } from "nativewind";
-const SCOPE = "ACCORDION";
+'use client';
+import React, { useMemo } from 'react';
+import { createAccordion } from '@gluestack-ui/accordion';
+import { Svg } from 'react-native-svg';
+import { View, Pressable, Text, Platform } from 'react-native';
+import { tva } from '@gluestack-ui/nativewind-utils/tva';
+import { withStyleContext, useStyleContext, } from '@gluestack-ui/nativewind-utils/withStyleContext';
+import { withStyleContextAndStates } from '@gluestack-ui/nativewind-utils/withStyleContextAndStates';
+import { H3 } from '@expo/html-elements';
+import { cssInterop } from 'nativewind';
+const SCOPE = 'ACCORDION';
 /** Styles */
 const accordionStyle = tva({
-    base: "w-full",
+    base: 'w-full',
     variants: {
         variant: {
-            filled: "bg-white",
+            filled: 'bg-white shadow-hard-2',
+            unfilled: '',
         },
         size: {
-            sm: "",
-            md: "",
-            lg: "",
+            sm: '',
+            md: '',
+            lg: '',
         },
     },
 });
 const accordionItemStyle = tva({
     parentVariants: {
         variant: {
-            filled: "bg-background-0",
-            unfilled: "bg-transparent",
+            filled: 'bg-background-0',
+            unfilled: 'bg-transparent',
         },
     },
 });
 const accordionTitleTextStyle = tva({
-    base: "text-typography-900 font-bold flex-1 text-left",
+    base: 'text-typography-900 font-bold flex-1 text-left',
     parentVariants: {
         size: {
-            sm: "text-sm",
-            md: "text-base",
-            lg: "text-lg",
+            sm: 'text-sm',
+            md: 'text-base',
+            lg: 'text-lg',
         },
     },
 });
 const accordionIconStyle = tva({
-    base: "",
+    base: 'text-typography-900 fill-none',
     parentVariants: {
         size: {
-            "2xs": "h-3 w-3",
-            xs: "h-3.5 w-3.5",
-            sm: "h-4 w-4",
-            md: "h-[18px] w-[18px]",
-            lg: "h-5 w-5",
-            xl: "h-6 w-6",
+            '2xs': 'h-3 w-3',
+            'xs': 'h-3.5 w-3.5',
+            'sm': 'h-4 w-4',
+            'md': 'h-[18px] w-[18px]',
+            'lg': 'h-5 w-5',
+            'xl': 'h-6 w-6',
         },
     },
 });
 const accordionContentTextStyle = tva({
-    base: "text-typography-700 font-normal",
+    base: 'text-typography-700 font-normal',
     parentVariants: {
         size: {
-            sm: "text-sm ",
-            md: "text-base",
-            lg: "text-lg",
+            sm: 'text-sm ',
+            md: 'text-base',
+            lg: 'text-lg',
         },
     },
 });
 const accordionHeaderStyle = tva({
-    base: "mx-0 my-0",
+    base: 'mx-0 my-0',
 });
 const accordionContentStyle = tva({
-    base: "px-5 mt-2 pb-5",
+    base: 'px-5 mt-2 pb-5',
 });
 const accordionTriggerStyle = tva({
-    base: "w-full py-5 px-5 flex-row justify-between items-center web:outline-none focus:outline-none data-[disabled=true]:opacity-40 data-[disabled=true]:cursor-not-allowed data-[focus-visible=true]:bg-background-50",
+    base: 'w-full py-5 px-5 flex-row justify-between items-center web:outline-none focus:outline-none data-[disabled=true]:opacity-40 data-[disabled=true]:cursor-not-allowed data-[focus-visible=true]:bg-background-50',
 });
-const PrimitiveIcon = React.forwardRef(({ height, width, fill = "none", color, size, as: AsComp, ...props }, ref) => {
+const PrimitiveIcon = React.forwardRef(({ height, width, fill, color, classNameColor, size, stroke, as: AsComp, ...props }, ref) => {
+    color = color ?? classNameColor;
     const sizeProps = useMemo(() => {
-        return size ? { size } : { height, width };
+        if (size)
+            return { size };
+        if (height && width)
+            return { height, width };
+        if (height)
+            return { height };
+        if (width)
+            return { width };
+        return {};
     }, [size, height, width]);
-    if (AsComp) {
-        return (<AsComp ref={ref} fill={fill} color={color} {...props} {...sizeProps}/>);
+    let colorProps = {};
+    if (fill) {
+        colorProps = { ...colorProps, fill: fill };
     }
-    return (<Svg ref={ref} height={height} width={width} fill={fill} color={color} {...props}/>);
+    if (stroke !== 'currentColor') {
+        colorProps = { ...colorProps, stroke: stroke };
+    }
+    else if (stroke === 'currentColor' && color !== undefined) {
+        colorProps = { ...colorProps, stroke: color };
+    }
+    if (AsComp) {
+        return <AsComp ref={ref} {...props} {...sizeProps} {...colorProps}/>;
+    }
+    return (<Svg ref={ref} height={height} width={width} {...colorProps} {...props}/>);
 });
-const Root = Platform.OS === "web"
+const Root = Platform.OS === 'web'
     ? withStyleContext(View, SCOPE)
     : withStyleContextAndStates(View, SCOPE);
-const Header = Platform.OS === "web" ? H3 : View;
+const Header = (Platform.OS === 'web' ? H3 : View);
 /** Creator */
 const UIAccordion = createAccordion({
     Root: Root,
     Item: View,
-    //@ts-ignore
     Header: Header,
     Trigger: Pressable,
     Icon: PrimitiveIcon,
@@ -98,29 +117,30 @@ const UIAccordion = createAccordion({
     ContentText: Text,
     Content: View,
 });
-cssInterop(UIAccordion, { className: "style" });
-cssInterop(UIAccordion.Item, { className: "style" });
-cssInterop(UIAccordion.Header, { className: "style" });
-cssInterop(UIAccordion.Trigger, { className: "style" });
-cssInterop(UIAccordion.Icon, { className: "style" });
-cssInterop(UIAccordion.TitleText, { className: "style" });
-cssInterop(UIAccordion.Content, { className: "style" });
-cssInterop(UIAccordion.ContentText, { className: "style" });
+cssInterop(UIAccordion, { className: 'style' });
+cssInterop(UIAccordion.Item, { className: 'style' });
+cssInterop(UIAccordion.Header, { className: 'style' });
+cssInterop(UIAccordion.Trigger, { className: 'style' });
+//@ts-ignore
+cssInterop(UIAccordion.Icon, { className: 'style' });
+cssInterop(UIAccordion.TitleText, { className: 'style' });
+cssInterop(UIAccordion.Content, { className: 'style' });
+cssInterop(UIAccordion.ContentText, { className: 'style' });
 // @ts-ignore
 cssInterop(UIAccordion.Icon, {
     className: {
-        target: "style",
+        target: 'style',
         nativeStyleToProp: {
-            height: "height",
-            width: "width",
-            //@ts-ignore
-            fill: "fill",
-            color: "color",
+            height: true,
+            width: true,
+            fill: true,
+            color: 'classNameColor',
+            stroke: true,
         },
     },
 });
 /** Components */
-const Accordion = React.forwardRef(({ className, variant = "filled", size = "md", ...props }, ref) => {
+const Accordion = React.forwardRef(({ className, variant = 'filled', size = 'md', ...props }, ref) => {
     return (<UIAccordion ref={ref} {...props} className={accordionStyle({ variant, class: className })} context={{ variant, size }}/>);
 });
 const AccordionItem = React.forwardRef(({ className, ...props }, ref) => {
@@ -142,16 +162,16 @@ const AccordionContentText = React.forwardRef(({ className, ...props }, ref) => 
             class: className,
         })}/>);
 });
-const AccordionIcon = React.forwardRef(({ size, className, color = "gray", ...props }, ref) => {
+const AccordionIcon = React.forwardRef(({ size, className, ...props }, ref) => {
     const { size: parentSize } = useStyleContext(SCOPE);
-    if (typeof size === "number") {
-        return (<UIAccordion.Icon ref={ref} {...props} color={color} className={accordionIconStyle({ class: className })} size={size}/>);
+    if (typeof size === 'number') {
+        return (<UIAccordion.Icon ref={ref} {...props} className={accordionIconStyle({ class: className })} size={size}/>);
     }
     else if ((props.height !== undefined || props.width !== undefined) &&
         size === undefined) {
-        return (<UIAccordion.Icon ref={ref} {...props} color={color} className={accordionIconStyle({ class: className })}/>);
+        return (<UIAccordion.Icon ref={ref} {...props} className={accordionIconStyle({ class: className })}/>);
     }
-    return (<UIAccordion.Icon ref={ref} color={color} {...props} className={accordionIconStyle({
+    return (<UIAccordion.Icon ref={ref} {...props} className={accordionIconStyle({
             size,
             class: className,
             parentVariants: { size: parentSize },
@@ -174,12 +194,12 @@ const AccordionTitleText = React.forwardRef(({ className, ...props }, ref) => {
             class: className,
         })}/>);
 });
-Accordion.displayName = "Accordion";
-AccordionItem.displayName = "AccordionItem";
-AccordionHeader.displayName = "AccordionHeader";
-AccordionTrigger.displayName = "AccordionTrigger";
-AccordionTitleText.displayName = "AccordionTitleText";
-AccordionContentText.displayName = "AccordionContentText";
-AccordionIcon.displayName = "AccordionIcon";
-AccordionContent.displayName = "AccordionContent";
+Accordion.displayName = 'Accordion';
+AccordionItem.displayName = 'AccordionItem';
+AccordionHeader.displayName = 'AccordionHeader';
+AccordionTrigger.displayName = 'AccordionTrigger';
+AccordionTitleText.displayName = 'AccordionTitleText';
+AccordionContentText.displayName = 'AccordionContentText';
+AccordionIcon.displayName = 'AccordionIcon';
+AccordionContent.displayName = 'AccordionContent';
 export { Accordion, AccordionItem, AccordionHeader, AccordionTrigger, AccordionTitleText, AccordionContentText, AccordionIcon, AccordionContent, };
